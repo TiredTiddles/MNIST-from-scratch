e0dd512a965c02229f57138be2b48cdd78d33edf
TiredTiddles <Johnny89762@gmail.com>
2026-07-14 19:56:20 +1000

    added batch training and epochs

--- main.py
+++ main.py
@@ -1,104 +1,97 @@
-from matplotlib import pyplot as plt
-
-import numpy as np
+import torch
 import pandas as pd
 
-training_data = pd.read_csv('data/train.csv').to_numpy()
-test_data = pd.read_csv('data/test.csv').to_numpy()
+training_data = torch.from_numpy(pd.read_csv('data/train.csv').to_numpy())
+training_test = torch.from_numpy(pd.read_csv('data/test.csv').to_numpy())
 
-print(len(training_data))
-np.random.shuffle(training_data)
+training_data = training_data.T
+m,n = training_data.shape
 
-X_train = training_data[:, 1:] / 255
-Y_train = training_data[:, 0]
-Y_train = np.eye(10)[Y_train]
+Y_train = torch.eye(10)[training_data[0]] # Y is now one hot encoded
+X_train = training_data[1:n] / 255
 
-m = 42000
+print(Y_train)
+print(X_train)
 
-X_sample = X_train[0:m].reshape(784,-1)
-Y_sample = Y_train[0:m].reshape(10,-1)
+# Initialise parameters
 
-print(X_sample.shape)
-print(Y_sample.shape)
+def init_params():
 
-def initialise_weights():
-    W1 = np.random.randn(10, 784) * np.sqrt(2.0 / 784)
-    b1 = np.zeros((10, 1))
-
-    W2 = np.random.randn(10, 10) * np.sqrt(2.0 / 10)
-    b2 = np.zeros((10, 1))
-
-    W3 = np.random.randn(10, 10) * np.sqrt(2.0 / 10)
-    b3 = np.zeros((10, 1))
+    W1 = torch.rand(10,784) - 0.5
+    b1 = torch.rand(10,1) - 0.5
+    W2 = torch.rand(10,10) - 0.5
+    b2 = torch.rand(10,1) - 0.5
+    W3 = torch.rand(10,10) - 0.5
+    b3 = torch.rand(10,1) - 0.5
 
     return W1, b1, W2, b2, W3, b3
 
-# Activation functions
-def ReLU(Z):
-    return np.maximum(0, Z)
+# Activation Functions
 
-def deriv_ReLU(Z):
-    return np.where(Z > 0, 1, 0)
+def ReLU(Z):
+    return torch.clamp(Z, min=0)
+
+def ReLU_deriv(Z):
+    return (Z > 0).float()
 
 def Softmax(Z):
-    Z_exp = np.exp(Z - np.max(Z))
-    A = Z_exp / (Z_exp.sum(axis=0, keepdims=True) + 1e-7)
+    A = torch.exp(Z) / torch.sum(torch.exp(Z), dim=0, keepdim=True)
     return A
 
-def forward_prop(W1, b1, W2, b2, W3, b3):
-    # layer 1
-    Z1 = W1 @ X_sample + b1
-    # print("W1:        ",W1.shape)
-    # print("X_sample:  ",X_sample.shape)
-    # print("b1:        ",b1.shape)
-    # print("Z1:        ",Z1.shape)
+# def Softmax_deriv(Z):
+#     return
+
+def forward_prop(W1, b1, W2, b2, W3, b3, X):
+
+    Z1 = W1 @ X + b1
     A1 = ReLU(Z1)
-    # print("A1:        ",A1.shape)
-    # print("\n")
+    print(f"Z1.shape: {Z1.shape}")
+    print(f"W1.shape: {W1.shape}")
+    print(f"X.shape:  {X.shape}")
+    print(f"b1.shape: {b1.shape}")
+    print(f"A1.shape: {A1.shape}")
+    print("\n")
 
-    # layer 2
     Z2 = W2 @ A1 + b2
-    # print("W2:        ",W2.shape)
-    # print("A2:        ",A1.shape)
-    # print("b2:        ",b2.shape)
-    # print("Z2:        ",Z2.shape)
     A2 = ReLU(Z2)
-    # print("A2:        ",A2.shape)
-    # print("\n")
+    print(f"Z2.shape: {Z2.shape}")
+    print(f"W2.shape: {W2.shape}")
+    print(f"A1.shape: {A1.shape}")
+    print(f"b2.shape: {b2.shape}")
+    print(f"A2.shape: {A2.shape}")
+    print("\n")
 
-    # layer 3
+    
     Z3 = W3 @ A2 + b3
-    # print("W3:        ",W3.shape)
-    # print("A2:        ",A2.shape)
-    # print("b3:        ",b3.shape)
-    # print("Z3:        ",Z3.shape)
     A3 = Softmax(Z3)
-    # print("A3:        ",A3.shape)
-    # print("\n")
+    print(f"Z3.shape: {Z3.shape}")
+    print(f"W3.shape: {W3.shape}")
+    print(f"A2.shape:  {A2.shape}")
+    print(f"b3.shape: {b3.shape}")
+    print(f"A3.shape: {A3.shape}")
+    print("\n")
 
     return Z1, A1, Z2, A2, Z3, A3
 
-def back_prop(Y_sample, X_sample, W1, W2, W3, A1, A2, A3, m):
-    # calculate loss
-    L = -(1/m) * (Y_sample * np.log(A3 + 1e-15)).sum()
+def back_prop(Z1, A1, W1, Z2, A2, W2, Z3, A3, W3, X, Y):
+    m = X.shape[1]
 
-    dZ3 = A3 - Y_sample
-    dW3 = (1/m) * dZ3 @ A2.T
-    db3 = (1/m) * np.sum(dZ3, axis=1, keepdims=True)
+    dZ3 = A3 - Y
+    dW3 = (1 / m) * dZ3 @ A2.T
+    db3 = (1 / m) * torch.sum(dZ3, dim=1, keepdim=True)
 
-    dA2 = np.dot(W3.T, dZ3)
-    dZ2 = dA2 * deriv_ReLU(A2)
-    dW2 = (1/m) * dZ2 @ A1.T
-    db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
+    dZ2 = (W3.T @ dZ3) * ReLU_deriv(Z2)
+    dW2 = (1 / m) * dZ2 @ A1.T
+    db2 = (1 / m) * torch.sum(dZ2, dim=1, keepdim=True)
 
-    dA1 = np.dot(W2.T, dZ2)
-    dZ1 = dA1 * deriv_ReLU(A1)
-    dW1 = (1/m) * dZ1 @ X_sample.T
-    db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
+    dZ1 = (W2.T @ dZ2) * ReLU_deriv(Z1)
+    dW1 = (1 / m) * dZ1 @ X.T
+    db1 = (1 / m) * torch.sum(dZ1, dim=1, keepdim=True)
 
-    return dW3, db3, dW2, db2, dW1, db1, L
+    return dW3, db3, dW2, db2, dW1, db1
 
-def update(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
+def update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
+
     W1 -= dW1 * alpha
     b1 -= db1 * alpha
     W2 -= dW2 * alpha
@@ -108,16 +101,12 @@ def update(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
 
     return W1, b1, W2, b2, W3, b3
 
-def gradient_descent(X_sample, Y_sample, epoches, m):
-    W1, b1, W2, b2, W3, b3 = initialise_weights()
-    for epoch in range(epoches):
-        Z1, A1, Z2, A2, Z3, A3 = forward_prop(W1, b1, W2, b2, W3, b3)
-        dW3, db3, dW2, db2, dW1, db1, L = back_prop(Y_sample, X_sample, W1, W2, W3, A1, A2, A3, m)
-        W1, b1, W2, b2, W3, b3 = update(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, 1e-2)
-        if epoch % 10 == 0:
-            print(f"Epoch {epoch} completed.")
-            print(f"Loss: {L}")
+W1, b1, W2, b2, W3, b3 = init_params()
+print('parameter initialisation complete!\n')
+Z1, A1, Z2, A2, Z3, A3 = forward_prop(W1, b1, W2, b2, W3, b3, X_train)
+print('forward propagation complete!\n')
+dW3, db3, dW2, db2, dW1, db1 = back_prop(Z1, A1, W1, Z2, A2, W2, Z3, A3, W3, X_train, Y_train)
+print('backward propagation complete!\n')
+W1, b1, W2, b2, W3, b3 = update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, 0.01)
+print('parameters updated!\n')
 
-    return W1, b1, W2, b2, W3, b3
-
-W1, b1, W2, b2, W3, b3 = gradient_descent(X_sample, Y_sample, 10001, m)
